e7971d259a2d3dd364c52e5dee79b043ed3cd5d4
codewithbg100 <connectedwithme100@gmail.com>
2023-09-22 09:01:08 -0700

    first commit

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+
+echo smtp_mailer('programmingaddicted110@gmail.com','Test Subject','Hello CodeWithBG');
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	//$mail->SMTPDebug = 2; 
+	$mail->Username = "connectedwithme100@gmail.com";
+	$mail->Password = "ybyv ouat plfb sgka";
+	$mail->SetFrom("connectedwithme100@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+?>
